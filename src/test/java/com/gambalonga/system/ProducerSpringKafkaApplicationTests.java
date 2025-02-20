9dcd63bac4c89f48e90955f23a5caa6aa5cd77a4
= <=>
2025-02-19 22:30:49 -0300

    Initial Commit

---
+++ src/test/java/com/gambalonga/system/ProducerSpringKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gambalonga.system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProducerSpringKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
